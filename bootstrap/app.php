afe59f6fae1357a634abf7470666927d0f398213
Diego200509 <diegojijon2005@gmail.com>
2025-10-29 18:54:19 -0500

    Se crearon los cors con fruitcake preinstalado.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->api(prepend: [
+            \Fruitcake\Cors\HandleCors::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
